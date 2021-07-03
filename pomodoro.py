982abc8675d4a0b2b7dda679c3ee463a9a9e05fb
Andrew John Roy Stephenson <andrewjohnroy@gmail.com>
2021-07-04 08:10:15 +1000

    first

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from time import sleep
+from playsound import playsound
 import _thread
 
 win = Tk()
@@ -11,22 +12,34 @@ win.geometry("300x140")
 label = Label(win, text="25:00", font=("Helvetica bold", 40))
 label.pack()
 
+started = False
+
 
 def timer():
+    global started
+    started = True
     if button["text"] == "Start":
         button["text"] = "Reset"
         seconds = 1500
-        while seconds >= 0:
-            label['text'] = str(int(seconds / 60)) + ":" + ("0" + str(seconds % 60) if seconds % 60 < 10 else
+        while seconds >= 0 and started:
+            label["text"] = str(int(seconds / 60)) + ":" + ("0" + str(seconds % 60) if seconds % 60 < 10 else
                                                             str(seconds % 60))
             sleep(1)
             seconds -= 1
+        playsound("zapsplat_bells_bell_small_hand_ring_ping_single_soft_003_67992.mp3")
+
+
+def start():
+    global started
+    if not started:
+        _thread.start_new_thread(timer, ())
     else:
-        label["text"] = "25:00"
+        started = False
         button["text"] = "Start"
+        label["text"] = "25:00"
 
 
-button = Button(win, text="Start", font=("Helvetica bold", 20), command=_thread.start_new_thread(timer()))
+button = Button(win, text="Start", font=("Helvetica bold", 20), command=start)
 button.pack()
 
 frame = Frame(win)
